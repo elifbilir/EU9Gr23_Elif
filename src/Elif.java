f0d6e3803e824078586eda2ba8f8664dc8c8a4c6
elifbasbug <elifbilir@gmail.com>
2022-08-10 20:56:11 +0100

    second commit from Elif

--- src/Elif.java
+++ src/Elif.java
@@ -1,5 +1,8 @@
 public class Elif {
     public static void main(String[] args) {
         System.out.println("first change from Elif");
+        System.out.println("second change from Elif");
+
+
     }
 }
